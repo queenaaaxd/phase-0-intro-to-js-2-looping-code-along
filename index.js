a20adddd78d6d54da9e28af1f5aff18d10fdf987
+ <+>
2023-03-01 18:57:40 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let message = []; // empty array to hold the messages
+     for (let i = 0; i < names.length; i++) {
+        message.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
